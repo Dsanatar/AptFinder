63b25ce59ef014ea51aeed27b15b22cf05c073a6
Zannzabar <danny.sanatar@gmail.com>
2025-03-24 17:35:02 -0400

    add potential to search multiple cities

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import configparser
-from zillow_api import get_listings
+from zillow_api import get_zillow
 from apt_scraper import scrape_apts
 from Apt import csv_dump
 from distance import get_distances
@@ -10,19 +10,23 @@ if __name__ == '__main__':
     config.read('config.ini')
 
     # grab user inputs out of ini
-    city = config['search']['city']
+    city_list = config['search']['city'].split(',')
     state = config['search']['state']
     is_rental = config.getboolean('search', 'is_rental')
     rent = int(config['search']['rent'])
     beds = int(config['search']['beds'])
     move_in_date = config['search']['move_in_date']
     calc_dist = config.getboolean('search', 'do_distance')
+    two_source = config.getboolean('search', 'do_both')
+
+    print('searching: ', city_list)
 
     apt_list = []
 
-    get_listings(city, state, is_rental, rent, beds, move_in_date, apt_list)
+    get_zillow(city_list, state, is_rental, rent, beds, move_in_date, apt_list)
 
-    scrape_apts(city, state, beds, rent, apt_list)
+    if two_source:
+        scrape_apts(city_list, state, beds, rent, apt_list)
 
     if calc_dist:
         get_distances(apt_list, state)
@@ -30,6 +34,6 @@ if __name__ == '__main__':
     #sort listings by rent
     #apt_list.sort(key=lambda x: x.rent)
 
-    csv_dump(apt_list)
+    csv_dump(apt_list, calc_dist)
 
 
--- zillow_api.py
+++ zillow_api.py
@@ -7,7 +7,7 @@ from Apt import Apt, csv_dump
 from distance import get_distances
 
 
-def get_listings(city, state, is_rental, rent, beds, move_in_date, apt_list):
+def get_zillow(city_list, state, is_rental, rent, beds, move_in_date, apt_list):
 
     url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
 
@@ -28,50 +28,55 @@ def get_listings(city, state, is_rental, rent, beds, move_in_date, apt_list):
     else:
         status_type = 'ForSale'
 
-    location = (city + ", " + state)
-
-    querystring = {
-                "location": location,
-                "status_type":status_type,
-                "rentMinPrice": rentMinPrice,
-                "rentMaxPrice": rentMaxPrice,
-                "bedsMin": bedsMin,
-    }
-    #print(querystring)
-
     env = dotenv_values('.env')
     headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
         "x-rapidapi-key": env["API_KEY"],
         "x-rapidapi-host": "zillow-com1.p.rapidapi.com"
     }
-    
-    response = requests.get(url, headers=headers, params=querystring)
 
-    data = response.json()
-    #print(data)
-    listings = data['props']
+    # make request for each city in config
+    for city in city_list:
+        location = (city.strip() + ", " + state)
+        print(location)
 
-    prefix = 'https://www.zillow.com'
+        querystring = {
+                    "location": location,
+                    "status_type":status_type,
+                    "rentMinPrice": rentMinPrice,
+                    "rentMaxPrice": rentMaxPrice,
+                    "bedsMin": bedsMin,
+                    "moveInDate": move_in_date
+        }
+        print(querystring)
+        
+        response = requests.get(url, headers=headers, params=querystring)
+        print(response)
 
-    for apt in listings:
-        #print(apt)
-        #skip for now
-        if 'units' in apt:
-            unit = apt['units'][0]
-            beds = unit['beds']
-            rent = unit['price']
-            rent = rent.replace('+', '')
-            rent = rent.replace('$', '')
-            rent = rent.replace(',', '')
-        else:
-            rent = apt['price']
-            beds = apt['bedrooms']
-        beds = str(beds) + ' Beds'
-        address = apt['address']
-        url = prefix + apt['detailUrl']
-        a = Apt(rent, beds, address, "", url)
-        apt_list.append(a)
+        data = response.json()
+        #print(data)
+        listings = data['props']
 
+        prefix = 'https://www.zillow.com'
+
+        for apt in listings:
+            # response is different if listing has multiple units
+            if 'units' in apt:
+                unit = apt['units'][0]
+                beds = unit['beds']
+                rent = unit['price']
+                rent = rent.replace('+', '')
+                rent = rent.replace('$', '')
+                rent = rent.replace(',', '')
+            else:
+                rent = apt['price']
+                beds = apt['bedrooms']
+            beds = str(beds) + ' Beds'
+            address = apt['address']
+            url = prefix + apt['detailUrl']
+            a = Apt(rent, beds, address, "", url)
+            apt_list.append(a)
+            
 
     print('Found', len(apt_list))
     #get_distances(apt_list, state)
